9aae13212f98d58bd9ebc5308f895a2bb93ef0c9
jscholar <jinruncai@gmail.com>
2019-08-06 23:43:36 -0700

    Cells can be visited and should reference neighbors

--- Maze Generator/app.js
+++ Maze Generator/app.js
@@ -1,18 +1,19 @@
-var cols, rows;
+const size = 400;
 const w = 40;
-
-var grid = [];
-
+const grid = [];
+var current;
+var rows, cols;
 function setup() {
-    createCanvas(400, 400);
-    cols = floor(width/w);
-    rows = floor(height/w);
+    createCanvas(size, size);
+    cols = floor(size/w);
+    rows = floor(size/w);
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             var cell = new Cell(i,j);
             grid.push(cell);
         }
     }
+    current = grid[0];
 }
 
 function draw() {
@@ -20,6 +21,7 @@ function draw() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
     }
+    current.visited = true;
 }
 
 window.grid = grid;
